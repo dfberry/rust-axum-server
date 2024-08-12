efda94cda8b2da71cd42e3aad1f0231a0ebef9e9
Dina Berry <dinaberry@outlook.com>
2024-08-12 16:23:08 -0700

    update create-resources.sh

--- scripts/create-resources.sh
+++ scripts/create-resources.sh
@@ -4,13 +4,15 @@
 ## az login --use-device-code
 ##
 
-# Set variables
-AZURE_SUBSCRIPTION_ID="your-subscription-id"
-AZURE_RESOURCE_GROUP="your-resource-group"
-AZURE_LOCATION="your-location"
+# Set variables - onlu use alphanumeric characters (no dashes or underscores)
+AZURE_SUBSCRIPTION_ID="<your-subscription-id>"
+AZURE_RESOURCE_GROUP="<your-resource-group>"
+AZURE_LOCATION="<your-location>"
 
-AZURE_CONTAINER_REGISTRY_NAME="your-acr-name"
-AZURE_CONTAINER_APP_NAME="your-container-app-name"
+AZURE_CONTAINER_REGISTRY_NAME="<your-container-registry-name>"
+AZURE_CONTAINER_APP_ENV_NAME="<your-container-app-env-name>"
+AZURE_CONTAINER_APP_NAME="<your-container-app-name>"
+
 
 # Create Azure resource group
 az group create --subscription $AZURE_SUBSCRIPTION_ID --name $AZURE_RESOURCE_GROUP --location $AZURE_LOCATION
@@ -18,5 +20,9 @@ az group create --subscription $AZURE_SUBSCRIPTION_ID --name $AZURE_RESOURCE_GRO
 # Create Azure container registry
 az acr create --subscription $AZURE_SUBSCRIPTION_ID --resource-group $AZURE_RESOURCE_GROUP --name $AZURE_CONTAINER_REGISTRY_NAME --sku Basic
 
+# Create Azure Container App environment
+az containerapp env create --subscription $AZURE_SUBSCRIPTION_ID --resource-group $AZURE_RESOURCE_GROUP --name $AZURE_CONTAINER_APP_ENV_NAME --location $AZURE_LOCATION
+
+
 # Create Azure container app
-az containerapp create --subscription $AZURE_SUBSCRIPTION_ID --resource-group $AZURE_RESOURCE_GROUP --name $AZURE_CONTAINER_APP_NAME --environment ${AZURE_CONTAINER_APP_NAME}-env
+az containerapp create --subscription $AZURE_SUBSCRIPTION_ID --resource-group $AZURE_RESOURCE_GROUP --name $AZURE_CONTAINER_APP_NAME --environment ${AZURE_CONTAINER_APP_ENV_NAME}
