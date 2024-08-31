e1591b9ca6a756a4aec4332541c7253d5b230bfc
Dina Berry <dinaberry@outlook.com>
2024-08-31 22:04:24 +0000

    first api test

---
+++ tests/src/__tests__/config.spec.ts
@@ -0,0 +1,28 @@
+import superagent from 'superagent';
+import { describe, it, expect } from '@jest/globals';
+import 'dotenv/config';
+
+const BASE_URL = process.env['BASE_URL'] || 'http://localhost:4000';
+
+describe('API Config', () => {
+  it('should get API config JSON', async () => {
+    const response = await fetch(`${BASE_URL}/config`, {method: 'GET'});
+
+    superagent
+    .get(`${BASE_URL}/config`)
+    .end((err, res) => {
+      expect(response.status).toBe(200);
+
+      // // Verify headers contain x-source-board-version
+      // expect(response.headers).toHaveProperty('x-source-board-version');
+  
+      // // Make sure the response has app_state and env_vars properties
+      // response.json().then((data) => {
+      //   expect(data).toHaveProperty('app_state');
+      //   expect(data).toHaveProperty('env_vars');
+      // });
+    });
+
+
+  });
+});
