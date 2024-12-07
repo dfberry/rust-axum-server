2aea778d1c88e7480b0a36dfce32fd13c37cf3c1
Dina Berry <dinaberry@outlook.com>
2024-12-07 16:15:33 +0000

    0.5.4 fix - /generate/uniqueid as JSON

--- src/routes/generate.rs
+++ src/routes/generate.rs
@@ -1,9 +1,12 @@
-use serde::Deserialize;
-use axum::Json;
+use axum::{
+    Json,
+    response::{IntoResponse, Response},
+    http::{self, StatusCode},
+    body::Body,
+};
 use crate::utils::generate::generate_unique_id;
-use axum::response::{IntoResponse, Response};
-use axum::http::{self, StatusCode};
-use axum::body::Body;
+use serde::Deserialize;
+use serde_json::json;
 
 #[derive(Deserialize)]
 pub struct GenerateUniqueIdRequestBody {
@@ -21,10 +24,17 @@ pub async fn handler_generate_unique_id(
     let unique_id: String = generate_unique_id(length);
     println!("Generated unique id: {}", unique_id);
 
+    let returned_json = json!({
+        "unique_id": unique_id,
+        "length": length
+    });
+
+    println!("Returned JSON: {}", returned_json.to_string());
+
     Response::builder()
         .header(http::header::CONTENT_TYPE, "application/json")
         .status(StatusCode::OK)
-        .body(Body::from(unique_id))
+        .body(Body::from(returned_json.to_string()))
         .unwrap()
 }
 
